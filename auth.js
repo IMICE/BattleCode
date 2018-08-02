25e2ef71d5ff76a89c30c9f8f58e9e4f8f8f5c11
alecgilchrist <alecgilchrist@gmail.com>
2018-08-02 15:51:34 -0500

    Auth switch

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const GoogleAuth = require('google-auth-library');
 
 const auth = new GoogleAuth;
-const CLIENT_ID = '106454631553-mles8i7ktt96qbvps7uoh2k9idop90e0.apps.googleusercontent.com';
+const CLIENT_ID = '211293983578-31ep5qbej79oebntrtn4rd3nbtqvemqc.apps.googleusercontent.com';
 const client = new auth.OAuth2(CLIENT_ID);
 exports.tokenCheck = (token, cb) => {
   client.verifyIdToken(
@@ -16,4 +16,3 @@ exports.tokenCheck = (token, cb) => {
     },
   );
 };
-
