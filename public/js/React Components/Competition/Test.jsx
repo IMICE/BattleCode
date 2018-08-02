05bd42608fa48628f5c834ed56ae3b7fcccf481b
Paul Marinaro <paulmarinaro@gmail.com>
2018-08-02 13:20:45 -0500

    looking for shake bug

--- public/js/React Components/Competition/Test.jsx
+++ public/js/React Components/Competition/Test.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardText } from 'material-ui';
-import axios from 'axios';
+// import axios from 'axios';
 
 export default class Test extends Component {
   componentDidMount() {
-    
-
     try {
       mocha.suite.suites.splice(0, 1);
       eval(`${this.props.userInput}; ${this.props.test};`);
@@ -22,8 +20,7 @@ export default class Test extends Component {
 
       setTimeout(() => {
         if (mocha.suite.suites[0].tests.every(test => test.state === 'passed')) {
-
-          // document.getElementsByClassName('Confetti')[0].style.display = 'block';
+          document.getElementsByClassName('Confetti')[0].style.display = 'block';
           if (this.props.passed === false) {
             this.props.update();
           }
@@ -50,7 +47,7 @@ export default class Test extends Component {
 Test.propTypes = {
   test: PropTypes.string.isRequired,
   userInput: PropTypes.string.isRequired,
-  user: PropTypes.string.isRequired,
-  testId: PropTypes.string.isRequired,
+  // user: PropTypes.string.isRequired,
+  // testId: PropTypes.string.isRequired,
   // compDescState: PropTypes.function.isRequired,
 };
