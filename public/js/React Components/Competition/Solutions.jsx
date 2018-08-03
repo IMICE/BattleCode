2a4f50f40293fd6c7e46c0b59af7080394007f24
alecgilchrist <alecgilchrist@gmail.com>
2018-08-03 11:46:16 -0500

    styling

--- public/js/React Components/Competition/Solutions.jsx
+++ public/js/React Components/Competition/Solutions.jsx
@@ -12,28 +12,28 @@ class Solutions extends Component {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            
-      <Card className = "Card">
-        <CardText> 
-        
-          <div><b>Good work solving all the tests </b></div>
-          <div><b>Time: {this.props.time}s Points: {this.props.points}</b></div>
-          <div>Here are some previous solutions</div>
-          <div>
-          
-          {this.props.solutions.map(solution => <SolutionsList solution={solution} key={solution._id} />)}
-          
-          </div>
-        </CardText>
-        {/* <CardActions>
+
+            <Card className="Card">
+              <CardText>
+
+                <div><b>Good work solving all the tests </b></div>
+                <div><b>Time: {this.props.time}s Points: {this.props.points}</b></div>
+                <div>Here are some previous solutions</div>
+                <div>
+
+                  {this.props.solutions.map(solution => <SolutionsList solution={solution} key={solution._id} />)}
+
+                </div>
+              </CardText>
+              {/* <CardActions>
       <FlatButton label="Action1" />
       <FlatButton label="Action2" />
     </CardActions> */}
-      </Card>
+            </Card>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-      
+
     );
   }
 }
