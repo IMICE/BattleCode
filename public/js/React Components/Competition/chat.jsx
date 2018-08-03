88cfa85e3e57a72752bf089c6b0c9c94348995a1
Josh <joshuablankenshipnola@gmail.com>
2018-08-03 12:46:35 -0500

    set up axios cal for giphy

--- public/js/React Components/Competition/chat.jsx
+++ public/js/React Components/Competition/chat.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import axios from 'axios';
 
 export default class Chat extends Component {
   constructor(props) {
@@ -8,6 +9,9 @@ export default class Chat extends Component {
     this.state = {
       message: '',
       messages: [],
+      GIF: '',
+      gifmessage: '',
+      query: '',
     };
     this.socket = io();
 
@@ -22,8 +26,9 @@ export default class Chat extends Component {
     this.handleKeyInput = (e) => {
       if (e.key === 'Enter') {
         e.preventDefault();
-        this.sendMessage(e)
+        this.sendMessage(e);
       }
+      // this.getGif('poop');
     };
 
     this.sendMessage = (ev) => {
@@ -34,6 +39,25 @@ export default class Chat extends Component {
       });
       this.setState({ message: '' });
     };
+    this.getGif = (query) => {
+      const api_key= 'cipCnvIV4PjYoVFWI4y299aXO62gyxy2';
+      axios.get(`http://api.giphy.com/v1/gifs/search?q=poop&api_key=${api_key}`)
+        .then((response) => {
+          let results = response.data.data[0];
+            this.setState({
+              GIF: results.images.downsized.url,
+            });
+          //  else {
+          //   this.setState({
+          //     gifmessage: 'No GIFs found'
+          //   })
+          // }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+    
   }
   render() {
     return (
@@ -44,20 +68,24 @@ export default class Chat extends Component {
               <div className="card-body">
                 <div className="card-title">Global Chat</div>
                 <hr />
+                {/* <img src={this.state.GIF}></img> */}
                 <div className="messages">
                   {this.state.messages.map(message => (
                     <div>{message.author}: {message.message}</div>
                   ))}
                 </div>
-
               </div>
               <div className="card-footer">
+              
                 <br />
                 <input
                   onKeyPress={this.handleKeyInput}
                   type="text"
                   placeholder="Message"
-                  className="form-control" value={this.state.message} onChange={ev => this.setState({ message: ev.target.value })} />
+                  className="form-control"
+value={this.state.message}
+onChange={ev => this.setState({ message: ev.target.value })}
+                />
                 <br />
                 <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
               </div>
